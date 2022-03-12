33f6f0c7efc2a8165d672424363c438bfa95ee3e
fabiola barrios <fabi.barrios23@gmail.com>
2022-03-12 16:15:53 -0300

    Clase 5 - Componentes II

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
+
+  
+
   return (
     <div className="App">
       <Navbar/>
       <ItemListContainer greeting='Hola somos Eureka'/>
+      <ItemCount stock={5} initial={5} onAdd={(number) => {alert(number)}}/>
+      <ItemCount stock={5} initial={5} onAdd={(number) => {alert("Hola la compra es:" + number)}}/>
     </div>
       
         
---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './ItemCount.css';
+import { useState } from 'react';
+
+function ItemCount({ stock = 10, initial = 1, onAdd}) {
+
+    const [count, setCount] = useState(initial)
+
+    const substractCount = () => {
+        if (count > 0) {
+            setCount(count - 1)
+        }
+    } //Función para restar al count
+
+    const addCount = () => {
+        if (count < stock) {
+            setCount(count + 1)
+        }
+    } //Función para sumar al count
+
+    const submitOnAdd = () => {
+        if (stock > 0) {
+            onAdd(count);
+        }
+        else {
+            alert ("No hay stock")
+        }
+        
+    }
+
+    
+
+
+    return (
+        <>
+            <div className="card" >
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div div className="input-group w-auto justify-content-end align-items-center" >
+                        <input onClick={substractCount} type="button" value="-" className="button-minus border rounded-circle  icon-shape icon-sm mx-1 " data-field="quantity" />
+                        <input type="number" step="1" max="10" value={count} name="quantity" className="quantity-field border-0 text-center w-25" />
+                        <input onClick={addCount} type="button" value="+" className="button-plus border rounded-circle icon-shape icon-sm " data-field="quantity" />
+                    </div >
+                    <a href="#" onClick={submitOnAdd} className="btn btn-primary ">Go somewhere</a>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ItemCount
+
